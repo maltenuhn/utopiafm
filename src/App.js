806ee8a50b26c99fee8db07bcde77285f8c700a8
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-10-01 11:13:14 +0200

    removing Recoil

--- src/App.js
+++ src/App.js
@@ -1,41 +1,5 @@
 import React from "react";
-import "./styles.css";
-import { Global } from "@emotion/core";
 import styled from "@emotion/styled";
-
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue
-} from "recoil";
-
-// State
-
-const playerStatus = atom({
-  key: "playerStatus",
-  default: "paused"
-});
-const selectedSongID = atom({
-  key: "song",
-  default: null
-});
-
-const selectedChannel = atom({
-  key: "selectedChannel",
-  default: null
-});
-
-const currentSong = atom({
-  key: "currentSong",
-  default: null
-});
-
-const nextUpSongs = atom({
-  key: "nextUpSongs",
-  default: [5, 3, 6]
-});
 
 const songs = [
   { title: "Utopia", singer: "Austra", duration: "3:39" },
@@ -45,30 +9,26 @@ const songs = [
   { title: "One of Two", singer: "Breakbot", duration: "13:39" },
   { title: "You've got me", singer: "Yung Bae", duration: "3:05" },
   { title: "Best Of You", singer: "Get To Know", duration: "3:15" },
-  { title: "Raphael Futura", singer: "Riviera", duration: "3:02" }
+  { title: "Raphael Futura", singer: "Riviera", duration: "3:02" },
 ];
 
-const channelList = atom({
-  key: "channelList",
-  default: [
-    "Utopia FM (default)",
-    "Indie Summer",
-    "Hangover Club",
-    "Tokyo Disco",
-    "Friday Nite Heat"
-  ]
-});
+const channelList = [
+  "Utopia FM (default)",
+  "Indie Summer",
+  "Hangover Club",
+  "Tokyo Disco",
+  "Friday Nite Heat",
+];
 
 const Header = styled.div({
   display: "flex",
   height: 12,
-  alignItems: "center"
+  alignItems: "center",
 });
 
 const ChannelSelector = (props) => {
   const [expanded, setExpanded] = React.useState(false);
-  const channels = useRecoilValue(channelList);
-  const [channel, setChannel] = useRecoilState(selectedChannel);
+  const [channel, setChannel] = React.useState("Utopia FM (default)");
 
   return (
     <div>
@@ -80,7 +40,7 @@ const ChannelSelector = (props) => {
           height: 30,
           display: "flex",
           alignItems: "center",
-          cursor: "pointer"
+          cursor: "pointer",
         }}
       >
         Channel:
@@ -94,12 +54,12 @@ const ChannelSelector = (props) => {
           boxShadow: "inset 0px 0px 0px 1px",
           backgroundColor: "white",
           height: !expanded ? "1px " : null,
-          overflow: "hidden"
+          overflow: "hidden",
         }}
       >
         {!expanded
           ? "nope"
-          : channels.map((x) => (
+          : channelList.map((x) => (
               <div
                 onClick={() => {
                   setExpanded(false);
@@ -110,7 +70,7 @@ const ChannelSelector = (props) => {
                   fontSize: 10,
                   padding: 8,
                   borderBottom: "1px dotted black",
-                  cursor: "pointer"
+                  cursor: "pointer",
                 }}
               >
                 {x}
@@ -135,24 +95,24 @@ const AppContainer = styled.div({
   padding: 4,
   boxShadow: "0px 0px 0px 0.5px rgba(0,0,0,.7)",
   backgroundColor: "#FFF6D9",
-  overflow: "hidden"
+  overflow: "hidden",
 });
 
 const ButtonGroup = styled.div({
   display: "flex",
   "& > *": {
     width: 40,
-    borderRight: "0px solid transparent"
+    borderRight: "0px solid transparent",
   },
   "& > *:first-child": {
     borderTopLeftRadius: 2,
-    borderBottomLeftRadius: 2
+    borderBottomLeftRadius: 2,
   },
   "& > *:last-child": {
     borderTopRightRadius: 2,
     borderBottomRightRadius: 2,
-    borderRight: "1px solid black"
-  }
+    borderRight: "1px solid black",
+  },
 });
 
 const Button = styled.button(
@@ -164,22 +124,22 @@ const Button = styled.button(
     "&:active": {
       background: "black",
       color: "#FFF6D9",
-      outline: "0px transparent"
-    }
+      outline: "0px transparent",
+    },
   },
   (props) => ({
     width: props.small ? 12 : null,
     height: props.small ? 12 : null,
     fontSize: props.small ? 8 : null,
     marginLeft: props.small ? 2 : null,
-    marginRight: props.small ? 2 : null
+    marginRight: props.small ? 2 : null,
   })
 );
 
 const Line = styled.div({
   height: "1px",
   width: "100%",
-  boxShadow: "inset 0px 0px 0px 1px black"
+  boxShadow: "inset 0px 0px 0px 1px black",
 });
 
 const SongDeets = (props) => (
@@ -217,96 +177,94 @@ export default function App() {
   };
 
   return (
-    <RecoilRoot>
-      <div style={{ fontFamily: "Krungthep", fontSize: 12, fontWeight: 300 }}>
-        <AppContainer>
-          <Header>
-            <Button small>♽</Button>
-            <Button small>⏁</Button>
-            <Button small>⍜</Button>
-            <div
-              style={{
-                height: 12,
-                fontSize: 8,
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-around"
-              }}
-            >
-              <Line />
-              <Line />
-              <Line />
-              <Line />
-              <Line />
-              <Line />
-            </div>
-            <span style={{ marginLeft: 8, marginRight: 4 }}>Utopia FM </span>
-          </Header>
+    <div style={{ fontFamily: "Krungthep", fontSize: 12, fontWeight: 300 }}>
+      <AppContainer>
+        <Header>
+          <Button small>♽</Button>
+          <Button small>⏁</Button>
+          <Button small>⍜</Button>
           <div
             style={{
+              height: 12,
+              fontSize: 8,
+              flexGrow: 1,
               display: "flex",
-              height: 26,
-              marginTop: 4,
-              backgroundColor: "black"
+              flexDirection: "column",
+              justifyContent: "space-around",
             }}
-          />
+          >
+            <Line />
+            <Line />
+            <Line />
+            <Line />
+            <Line />
+            <Line />
+          </div>
+          <span style={{ marginLeft: 8, marginRight: 4 }}>Utopia FM </span>
+        </Header>
+        <div
+          style={{
+            display: "flex",
+            height: 26,
+            marginTop: 4,
+            backgroundColor: "black",
+          }}
+        />
 
-          <ChannelSelector />
-          <div
-            style={{
-              height: 27,
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: 8,
-              paddingRight: 8
-            }}
-          >
-            <Timer time={"1:39"} duration={30} />
-            <span>{timeLeft}</span>
-          </div>
-          <div
-            style={{
-              height: 27,
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: 8,
-              paddingRight: 8
-            }}
-          >
-            <SongDeets song={playing} />
-          </div>
-          <div
-            style={{
-              marginTop: 20,
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: 8,
-              paddingRight: 8
-            }}
-          >
-            <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
-              <ButtonGroup>
-                <Button onClick={() => setPlaying(false)}>⏮</Button>
+        <ChannelSelector />
+        <div
+          style={{
+            height: 27,
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: 8,
+            paddingRight: 8,
+          }}
+        >
+          <Timer time={"1:39"} duration={30} />
+          <span>{timeLeft}</span>
+        </div>
+        <div
+          style={{
+            height: 27,
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: 8,
+            paddingRight: 8,
+          }}
+        >
+          <SongDeets song={playing} />
+        </div>
+        <div
+          style={{
+            marginTop: 20,
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: 8,
+            paddingRight: 8,
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
+            <ButtonGroup>
+              <Button onClick={() => setPlaying(false)}>⏮</Button>
 
-                <Button onClick={() => setPlaying(!playing)}>
-                  {playing ? "⏸" : "▶️"}
-                </Button>
-                <Button>⏭</Button>
-              </ButtonGroup>
-              <ButtonGroup>
-                <Button>♡</Button>
-              </ButtonGroup>
-              <ButtonGroup>
-                <Button>⎙</Button>
-              </ButtonGroup>
-              <ButtonGroup>
-                <Button>📲</Button>
-              </ButtonGroup>
-            </div>
+              <Button onClick={() => setPlaying(!playing)}>
+                {playing ? "⏸" : "▶️"}
+              </Button>
+              <Button>⏭</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button>♡</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button>⎙</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button>📲</Button>
+            </ButtonGroup>
           </div>
-        </AppContainer>
-      </div>
-    </RecoilRoot>
+        </div>
+      </AppContainer>
+    </div>
   );
 }
